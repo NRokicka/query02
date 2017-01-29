3a6584eb1765aa821eddb03ec978927470ffb164
Natalia Rokicka <natalia9rokicka@gmail.com>
2017-01-29 14:37:51 +0000

    add file

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-btn.addEventListener('click', function(e) {
-  var str = inpt.value;  // read value from input with id="str"
-  var res = str.split(' ').reverse().join(' '); // reverse symbols in a string
-  result.innerHTML = res; // write result to span with is="result"
-  e.preventDefault(); // prevent form submission
-});
+//DOM redy event 
+
+
+$(function() {
+  $('#btn').click(function(e){
+    e.preventDefault();
+    var str = $('#inpt').val();
+    var res = str.split(' ').reverse().join(' ');
+    $('#result').html(res);
+    $('p').addClass('done');
+  })
+ /* btn.addEventListener('click', function(e) {
+    e.preventDefault(); // prevent form submission
+    var str = inpt.value; // read value from input with id="str"
+    var res = str.split(' ').reverse().join(' '); // reverse symbols in a string
+    result.innerHTML = res; // write result to span with is="result"
+  });*/
+})
